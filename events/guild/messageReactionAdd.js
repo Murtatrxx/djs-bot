cfeca54e64ab0d40e5a25b17899aaff6271d5fa4
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2021-05-04 15:49:19 +0200

    messageReactionAdd Example
    - Add example to messageReactionAdd event

--- events/guild/messageReactionAdd.js
+++ events/guild/messageReactionAdd.js
@@ -1,3 +1,15 @@
-module.exports = () => {
+//Emoji's
+const example = '✅';
 
+
+module.exports = async (Discord, client, reaction, user) => {
+
+    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.fetch();
+    if (user.bot) return;
+    if (!reaction.message.guild) return;
+
+    if (reaction.emoji.name === example) {
+        //do stuff
+    }
 }
