3607884a030758455c1a209f84b9c6efcff968a1
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-04 21:00:26 +0530

    eval command test

--- commands/main/eval.js
+++ commands/main/eval.js
@@ -1,4 +1,18 @@
 const {NodeVM} = require('vm2');
 const vm = new NodeVM();
 
+module.exports = {
+    name: 'eval',
+    async execute(client, message, Discord){
+        let code = message.content.slice(5)
+        process.stdin.on('data', (data) => console.log(data))
+        try {
+            vm.run(code)
+        } catch (e) {
+            return message.reply(`\`\`\`js\n${e}\`\`\``)
+        }
+        message.reply('done')
+    }
+}
+
 //put this commands in main folder
