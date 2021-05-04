6475ffd9d7d528c8563cc5fc9dc406fa9c1878e3
Gautam BS <>
2021-05-04 13:40:58 +0000

    minor

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ module.exports = {
         message.channel.send("Pong!").then(m => {
             let ping = m.createdTimestamp - message.createdTimestamp
             let errembed = new MessageEmbed().setColor("RED").setTitle(":x: High ping has been detected on your server!").setDescription("You can try changing the region of the server to fix it").addField(":hourglass_flowing_sand: Ping:", ping + " ms", true).addField(":large_blue_diamond: Region:", message.guild.region, true).addField("Harmless Level:", ":yellow_circle::yellow_circle::yellow_circle::white_circle::white_circle: (Middle)");
-            const ememem = new MessageEmbed().setColor(color).addFields({ name: `**: hourglass: | Ping: ${ping}ms **`, value: `**: heartbeat: | API: ${bot.ws.ping}ms **` });
+            const ememem = new MessageEmbed().setColor('RED').addFields({ name: `**: hourglass: | Ping: ${ping}ms **`, value: `**: heartbeat: | API: ${bot.ws.ping}ms **` });
             m.delete();
             if (ping >= 1000) return message.channel.send(errembed)
             message.channel.send(ememem);
