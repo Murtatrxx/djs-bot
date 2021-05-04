08a2e4dfc946228d7d334705f3cd836de9718397
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-04 21:03:06 +0530

    minor

--- commands/main/eval.js
+++ commands/main/eval.js
@@ -5,13 +5,14 @@ module.exports = {
     name: 'eval',
     async execute(client, message, Discord){
         let code = message.content.slice(5)
-        process.stdin.on('data', (data) => console.log(data))
+        let logs = []
+        process.stdin.on('data', (data) => logs.push(data))
         try {
             vm.run(code)
         } catch (e) {
             return message.reply(`\`\`\`js\n${e}\`\`\``)
         }
-        message.reply('done')
+        message.reply("Logs:"+logs)
     }
 }
 
