8148a24a685622c5dfa41792988fbce0c5b39c87
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2021-05-04 16:30:51 +0200

    Update test.js

--- commands/example/test.js
+++ commands/example/test.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'test',
     execute(client, message, Discord) {
-        message.channel.send("test!")
+        message.channel.send("HEY i need to send :test!")
     }
 }
