5d05145e15d3ad367b298781299ce6eb3ce9f369
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-04 21:54:02 +0530

    eval command test002

--- commands/main/eval.js
+++ commands/main/eval.js
@@ -1,5 +1,15 @@
 const {NodeVM} = require('vm2');
+const assert = require('assert');
+const { log } = require('util');
 const vm = new NodeVM();
+
+let logs = [], warns = [], errors = [];
+
+const logger = {
+    log: (...data) => logs.push(...data),
+    warn: (...data) => warns.push(...data),
+    error: (...data) => errors.push(...data)
+}
 
 module.exports = {
     name: 'eval',
@@ -8,11 +18,14 @@ module.exports = {
         let logs = []
         process.stdin.on('data', (data) => logs.push(data))
         try {
+            assert.ok(vm.run(`console.log`) === logger.log);
+            assert.ok(vm.run(`console.error`) === logger.error);
+            assert.ok(vm.run(`console.warn`) === logger.warn);
             vm.run(code)
         } catch (e) {
             return message.reply(`\`\`\`js\n${e}\`\`\``)
         }
-        message.reply("Logs:"+logs)
+        message.reply("Logs:"+logs+warns+errors)
     }
 }
 
