1629ec905095f8b7daec4104bb39e9ca9a345284
Gautam BS <>
2021-05-04 14:18:11 +0000

    debug

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -8,6 +8,7 @@ module.exports = (client, Discord) => {
       const command = require(`../commands/${dirs}/${file}`)
       if (command.name) {
         client.commands.set(command.name, command);
+				console.log('test')
       } else continue
     }
   }
