2d7a3315031b556d167aeae1a3754a292c1b0aef
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-05 15:58:43 +0530

    fix

--- events/guild/message.js
+++ events/guild/message.js
@@ -7,7 +7,7 @@ module.exports = (client, message) => {
     if (!message.content.startsWith(prefix) || message.author.bot) return
 
     const { member, content } = message
-    const args = content.toLowerCase().split(/\s+/);
+    const args = content.toLowerCase().slice(prefix.length).split(/\s+/);
     const cmd = args.shift();
 
     const command = client.commands.get(cmd);
