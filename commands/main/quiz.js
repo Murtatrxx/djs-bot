183bf6165d0584e753a474ce359debbd642f64eb
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-05 20:26:15 +0530

    .

--- commands/main/quiz.js
+++ commands/main/quiz.js
@@ -35,7 +35,10 @@ module.exports = {
       msg.react('✅')
       msg.react('❌')
       let cltr = await msg.awaitReactions(((r, u) => u.id === message.author.id && !u.bot && r.emoji.name === ('✅' || '❌')), { time: 60000, max: 1, errors: ['time'] }).catch(e => msg.edit("", { embed: embed.setDescription('**Quiz cancelled**').setColor('RED')}))
-			if (cltr.first().emoji.name === '❌') return msg.edit("", { embed: embed.setDescription('**Quiz cancelled**').setColor('RED')})
+			if (cltr.first().emoji.name === '❌') {
+        msg.reactions.removeAll()
+        return msg.edit("", { embed: embed.setDescription('**Quiz cancelled**').setColor('RED')})
+      }
       else msg.reactions.removeAll()
 
 
@@ -49,6 +52,7 @@ module.exports = {
 
       collector.on('collect', (reaction, user) => {
         reaction.users.remove(user.id)
+        console.log(sts.qn, sts.score)
         switch (reaction.emoji.name) {
           case '1️⃣':
             skip(msg)
