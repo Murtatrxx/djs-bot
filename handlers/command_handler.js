8fa560821365e39d2d5d69e66b3ce57dd0205bc0
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-05 15:55:17 +0530

    test003

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -3,8 +3,9 @@ const fs = require("fs");
 module.exports = (client, Discord) => {
   const loaddirs = (dirs) => {
     const command_files = fs.readdirSync(`./commands/${dirs}`).filter(file => file.endsWith('.js'));
-
+    
     for (const file of command_files) {
+      console.log(file)
       const command = require(`../commands/${dirs}/${file}`)
       if (command.name) {
         client.commands.set(command.name, command);
