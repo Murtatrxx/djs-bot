f286d082006c991828b864804f97227af00ac32c
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-05 06:30:44 +0530

    .

--- commands/main/eval.js
+++ commands/main/eval.js
@@ -2,20 +2,20 @@ const { VM } = require('vm2');
 const assert = require('assert');
 const { log } = require('util');
 const vm = new NodeVM();
+let logs = []
 const log = (...data) => logs.push(...data)
 
 module.exports = {
     name: 'eval',
     async execute(client, message, Discord){
         let code = message.content.slice(5)
-        let logs = []
         try {
             assert.ok(vm.run(`console.log`) === log);
             vm.run(code)
         } catch (e) {
             return message.reply(`\`\`\`js\n${e}\`\`\``)
         }finally {
-            message.reply("Not yet implemented"+log)
+            message.reply("Not yet implemented"+logs)
         }
     }
 }
