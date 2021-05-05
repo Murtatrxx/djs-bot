d8651a4089562d4d73ea667915bce6715b1ac519
heynaberuy <>
2021-05-05 08:50:24 +0000

    edit01

--- commands/main/quiz.js
+++ commands/main/quiz.js
@@ -1,12 +1,20 @@
-const fetch = require('node-fetch')
-const key = require('../../config.js').quiz
+const fetch = require('node-fetch');
+const key = require('../../config.js').quiz;
+const { MessageEmbed } = require('discord.js')
+
 module.exports = {
-    name: 'quiz',
-    async execute(client, message, args) {
-        let arr = []
-        fetch(`https://quizapi.io/api/v1/questions?apiKey=${key}`).then(res => res.json()).then(result => {
-            result.forEach(m => arr.push({ question: m.question, options: [m.answers.answer_a, m.answers.answer_b, m.answers.answer_c, m.answers.answer_d, m.answers.answer_e, m.answers.answer_f], correctIndex: (Object.values(m.correct_answers).findIndex(e => e === true)) }))
-            message.ireply(`${arr[0].question}\n${arr[0].options.filter(m => m).map((m, i) => `${i+1}. ${m}`).join('\n')}`)
-        })
-    }
+  name: 'quiz',
+  async execute(client, message, args) {
+    let arr = [];
+    fetch(`https://quizapi.io/api/v1/questions?apiKey=${key}`).then(res => res.json()).then(result => {
+      result.forEach(m => arr.push({ question: m.question, options: [m.answers.answer_a, m.answers.answer_b, m.answers.answer_c, m.answers.answer_d, m.answers.answer_e, m.answers.answer_f], correctIndex: (Object.values(m.correct_answers).findIndex(e => e === true)) }));
+      
+      let embed = new MessageEmbed()
+        .setTitle("HTML Quiz")
+        .setColor("BLACK")
+        .setDescription(`${arr[0].question}\n${arr[0].options.filter(m => m).map((m, i) => ${i + 1}. ${m}`).join('\n')}`)
+        .setFooter("✅ Correct 0/10")
+      message.ireply(embed)
+    })
+  }
 }
