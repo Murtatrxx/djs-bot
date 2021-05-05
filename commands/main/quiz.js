b8b4fbc426463ea917bcaec4152429541c554625
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-05 11:20:37 +0530

    test006

--- commands/main/quiz.js
+++ commands/main/quiz.js
@@ -5,9 +5,9 @@ module.exports = {
     async execute(client, message, args) {
         let arr = []
         fetch(`https://quizapi.io/api/v1/questions?apiKey=${key}`).then(res => res.json()).then(result => {
-            console.log(result)
             result.forEach(m => arr.push({ question: m.question, options: [m.answers.map(m => m)], correctIndex: (m.correct_answers.findIndex(e => e === true)) }))
         })
+        console.log(arr)
         message.ireply(`${arr[0].question}\n${arr[0].options.map((m, i) => `${i+1}. ${m}`)}`)
     }
 }
