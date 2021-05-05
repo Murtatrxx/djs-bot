e068bf25548b9780d503dc89e85864fb68257ba7
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-05 11:09:52 +0530

    minor

--- commands/main/quiz.js
+++ commands/main/quiz.js
@@ -3,7 +3,9 @@ const key = require('../../config.js').quiz
 module.exports = {
     name: 'quiz',
     async execute(client, message, args) {
-        fetch(`https://quizapi.io/api/v1/questions?apiKey=${key}&limit=20`)
-        .then(res => res.text()).then(r => message.channel.send(JSON.stringify(r),{ split: {char: ''}}))
+        let arr = []
+        (await fetch(`https://quizapi.io/api/v1/questions?apiKey=${key}&limit=20`).then(res => res.json()))
+            .forEach(m => arr.push({ question: m.question, options: [m.answers.map(m => m)], correctIndex: (m.correct_answers.findIndex(e => e === true)) }))
+        message.ireply(`${arr[0].question}\n${arr[0].options.map((m, i) => `${i+1}. ${m}`)}`)
     }
 }
