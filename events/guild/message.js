243d316fb21b1a0b1f134396ce9a02985ed06a24
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2021-05-05 11:30:06 +0200

    Fix

--- events/guild/message.js
+++ events/guild/message.js
@@ -9,16 +9,14 @@ module.exports = (Discord, client, message, validcommand) => {
 
     if (!message.content.startsWith(prefix) || message.author.bot) return
 
-    const args = message.content.slice(prefix.length).split(/ +/);
+    const args = message.content.split(/[ ]+/);
     const cmd = args.shift().toLowerCase();
 
     const command = client.commands.get(cmd);
 
     args.shift()
 
-    if (args.length < minArgs || (
-        args.content !== null && args.length > maxArgs
-    )) return message.channel.send(util.argumentMismatch)
+    if (args.length < minArgs || (args.content !== null && args.length > maxArgs)) return message.channel.send(util.argumentMismatch)
 
     if (command) command.execute(client, message, args);
 }
