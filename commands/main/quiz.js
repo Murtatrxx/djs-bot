f629c2409d3047ca7cb2deb4202fd43e10d7b37f
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-05 10:17:36 +0530

    .

--- commands/main/quiz.js
+++ commands/main/quiz.js
@@ -3,7 +3,8 @@ const key = process.env.QUIZ_TOKEN
 module.exports = {
     name: 'quiz',
     async execute(client, message, args) {
-        fetch('https://quizapi.io/api/v1/questions?apiKey='+key,{ method: 'get', /*headers: {  'apiKey' : key }*/ })
+        console.log(key)
+        fetch(`https://quizapi.io/api/v1/questions?apiKey=${key}&limit=20`)
         .then(res => res.text()).then(r => message.ireply(JSON.stringify(r)))
     }
 }
