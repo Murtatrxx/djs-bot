6de86cab228178d75516e5e157f04c5940d5beec
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-05 09:20:10 +0530

    .

--- commands/main/quiz.js
+++ commands/main/quiz.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch')
 module.exports = {
     name: 'quiz',
     async execute ( ) {
-        fetch('https://quizapi.io/api/v1/questions',{ method: 'POST', body: 'apiKey='+process.env.QUIZ_TOKEN })
+        fetch('https://quizapi.io/api/v1/questions',{ method: 'POST', body: 'apiKey='+process.env.QUIZ_TOKEN, headers: { 'Content-Type': 'application/json' } })
         .then(res => res.text()).then(console.log)
     }
 }
