ee7b7dda536ca43a43f6a537c12a92c3af6039aa
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-05 10:03:16 +0530

    .

--- commands/main/quiz.js
+++ commands/main/quiz.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch')
 
 module.exports = {
     name: 'quiz',
-    async execute (client, message, args) {
+    async execute(client, message, args) {
         fetch('https://quizapi.io/api/v1/questions',{ method: 'get', headers: {  'X-Api-Key' : process.env.QUIZ_TOKEN } })
         .then(res => res.text()).then(r => message.send(JSON.stringify(r), { split: {char:''}}))
     }
