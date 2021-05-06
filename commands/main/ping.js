71ae1f2ccf45b5a1abf427cb6269c886b102b7cb
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-06 09:37:37 +0530

    prettified ping.js

--- commands/main/ping.js
+++ commands/main/ping.js
@@ -1,18 +1,22 @@
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-    name: 'ping',
-    minArgs: 0,
-    maxArgs: 0,
-    perms: ['ADMINISTRATOR'],
-    execute(client, message, args) {
-        message.channel.send("Pong!").then(m => {
-            let ping = m.createdTimestamp - message.createdTimestamp
-            const ememem = new MessageEmbed()
-                .setColor('PURPLE').addFields({ name: `**:hourglass: | Ping: ${ping}ms **`, value: `**:heartbeat: | API: ${client.ws.ping}ms **` });
-            m.delete();
-            if (ping >= 1000) return message.channel.send(errembed)
-            message.channel.send(ememem);
+  name: "ping",
+  minArgs: 0,
+  maxArgs: 0,
+  perms: ["ADMINISTRATOR"],
+  execute(client, message, args) {
+    message.channel.send("Pong!").then((m) => {
+      let ping = m.createdTimestamp - message.createdTimestamp;
+      const ememem = new MessageEmbed()
+        .setColor("PURPLE")
+        .addFields({
+          name: `**:hourglass: | Ping: ${ping}ms **`,
+          value: `**:heartbeat: | API: ${client.ws.ping}ms **`,
         });
-    }
-}
+      m.delete();
+      if (ping >= 1000) return message.channel.send(errembed);
+      message.channel.send(ememem);
+    });
+  },
+};
