3b4077b2d4424d2571213d17dcc75acacddeb0c6
heynaberuy <>
2021-05-07 10:29:27 +0000

    s

--- commands/main/codehelp.js
+++ commands/main/codehelp.js
@@ -1,8 +1,15 @@
-const error = require("../../utils/error.js")
+const error = require("../../utils/error.js");
+const { MessageEmbed } = require('discor.js');
 
 module.exports = {
   name: "codehelp",
   async execute(client, message, Discord) {
-    message.channel.send("ok").catch(e => error.send("Error:"+e.stack));
+    const args = message.content.split(/ +/)
+
+    let embed = new MessageEmbed()
+      .setTitle("Code Tutor for " + args[1])
+      .setDescription("Level " + args[2])
+      
+    message.channel.send(embed).catch(e => error.send("Error:"+e.stack));
   },
 }
