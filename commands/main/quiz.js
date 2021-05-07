dba38f1b4548737567e4d67c44a36e79a674049f
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-07 11:26:12 +0530

    test001

--- commands/main/quiz.js
+++ commands/main/quiz.js
@@ -1,7 +1,8 @@
 // @ts-check
 const fetch = require("node-fetch");
 const key = require("../../config.js").quiz;
-const { MessageEmbed, Collection } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const qoute = require("../../utils/quotes.json");
 
 const reactions = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"];
 
@@ -9,23 +10,27 @@ const reactions = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️�
 module.exports = {
   name: "quiz",
   async execute(client, message, args) {
+    //embed
     let embed = new MessageEmbed()
       .setTitle("Programming quiz")
       .setColor("BLUE")
-      .setDescription(
-        `**Are you ready to start the quiz?** React to ✅ to continue`
-      ).addFields({
+      .setDescription(`**Are you ready to start the quiz?** React to ✅ to continue`)
+      .addFields({
         name: 'Instructions',
         value: '● Read the questions.\n● There are 2 - 5 options for a question.\n● React to the message according to the Options\n● React with ❌ anytime to quit.'
       })
       .setFooter('You have 60s to react.')
       .setTimestamp();
-    let arr = [];
-    let score = 0, qn = 0;
+
+    //Meta
+    let arr = [], score = 0, qn = 0;
+
+    // For showing the answer
     const showans = async (msg, arr, extras = {}) => {
       let { exp, tip, diff, cat } = arr.meta, {rxn, user } = extras, color, des;
       msg.reactions.removeAll()
       msg.react('⏭️').catch(e => msg.react('⏭️').catch(console.error))
+      msg.react('❌')
       if(arr.correctIndex.includes(reactions.findIndex(f => f === rxn.emoji.name))) {
         score++
         color = "GREEN"
@@ -38,11 +43,10 @@ module.exports = {
       msg.edit("", { embed: embed})
       let cltr = await msg.awaitReactions((r, u) => !u.bot && r.emoji.name === '⏭️' && u.id === message.author.id,  { max: 1 })
       return msg;
-    }
+    };
+
   // @ts-ignore
-    fetch(
-      `https://quizapi.io/api/v1/questions?category=code&limit=10&apiKey=${key}`
-    )
+    fetch(`https://quizapi.io/api/v1/questions?category=code&limit=10&apiKey=${key}`)
       .then((res) => res.json())
       .then(async (result) => {
         result.forEach((m) => {
@@ -58,10 +62,24 @@ module.exports = {
           });
         });
 
-        // bool is for checking whether it's done by user or not.
-        const skip = (msg, extra = {}) => {
+        //Quit function 
+        const quit = (msg, { cltr }) => {
+          cltr.stop()
+          msg.reactions.removeAll()
+          embed.fields.splice(0, embed.fields.length)
+          embed.setColor("GREEN")
+            .setAuthor(message.member.displayName, message.author.displayAvatarURL())
+            .setFooter()
+            .setTimestamp()
+            .setTitle("Great attempt")
+            .setDescription()
+          msg.edit("", { embed: embed })
+        };
+
+        //For skiping the question
+        const skip = (msg, { collector }) => {
           msg.reactions.removeAll();
-          if (qn > 9) return extra.collector?.stop();
+          if (qn > 9) return quit(msg, { cltr: collector });
           qn++;
           embed.fields.splice(0, embed.fields.length);
           embed
@@ -71,29 +89,27 @@ module.exports = {
             .setDescription(`${arr[qn].question}`)
             .addFields({
               name: "Options",
-              value: arr[qn].options
-                .filter((m) => m)
-                .map((m, i) => `${i + 1}. ${m}`)
-                .join("\n"),
+              value: arr[qn].options.filter((m) => m).map((m, i) => `${i + 1}. ${m}`).join("\n"),
             });
           msg.edit("", { embed: embed });
+          msg.react('❌')
           arr[qn].options.filter((m) => m).forEach((m, index) => {
               msg.react(reactions[index]);
             });
         };
 
-
+        // First message
         let msg = await message.ireply("", { embed: embed });
         msg.react("✅");
         msg.react("❌");
 
         const filter = (r, u) => u.id === message.author.id && !u.bot && ["✅", "❌"].includes(r.emoji.name);
         let cltr = await msg.awaitReactions(filter, { time: 60000, max: 1, errors: ["time"] }).catch((e) =>{
-            msg.edit("", {
-              embed: embed.setDescription("**Quiz cancelled**").setColor("RED"),
-            });
-            msg.reactions.removeAll()
+          msg.reactions.removeAll()
+          msg.edit("", {
+            embed: embed.setDescription("**Quiz cancelled**").setColor("RED"),
           });
+        });
 
         if (cltr.first().emoji.name === "❌") {
           msg.reactions.removeAll()
@@ -102,21 +118,12 @@ module.exports = {
           });
         } else msg.reactions.removeAll()
 
+        // For removing the instructions field
         embed.fields.splice(0, embed.fields.length)
-        // embed
-        //   .setFooter(`Question ${qn + 1}/10 • Score: ${score}`)
-        //   .setColor("BLUE")
-        //   .setDescription(`${arr[0].question}`)
-        //   .addFields({
-        //     name: "Options",
-        //     value: arr[0].options.filter((m) => m).map((m, i) => `${i + 1}. ${m}`).join("\n"),
-        //   });
-
-        // arr[qn].options.filter((m) => m).forEach((m, index) => msg.react(reactions[index]));
         skip(msg)
 
-        msg.edit("", { embed: embed });
-        const collector = msg.createReactionCollector((r, u) => u.id === message.author.id && !u.bot && reactions.includes(r.emoji.name));
+        // msg.edit("", { embed: embed });
+        const collector = msg.createReactionCollector((r, u) => u.id === message.author.id && !u.bot && (reactions.includes(r.emoji.name) || r.emoji.name === '❌'));
 
         collector.on("collect", (reaction, user) => showans(msg, arr[qn], { rxn: reaction, user }).then(m => skip(m, { collector })));
       });
