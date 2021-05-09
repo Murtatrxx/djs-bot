cb72584a3ac753916da2eb7b855b5733bb105582
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-09 16:44:33 +0530

    minor

--- commands/main/docs.js
+++ commands/main/docs.js
@@ -6,15 +6,23 @@ module.exports = {
   minArgs: 1,
   maxArgs: 2,
   perms: [""],
-  execute(client, message, args) {
+  async execute(client, message, args) {
     try {
-        let source = args.find(m => /^(--src|-s)\w*$/ig.test(m.trim().toLowerCase()))?.replace(/^(--src|-s)(\w*)$/, "$2") ?? "stable";
-        let poss = ["master", "stable", "collection", "commando", "rpc", "akairo", "akairo-master"]
-        fetch(`https://djsdocs.sorta.moe/v2/embed?src=${source}&q=${args[0]}`)
-            .then(res => res.json()).catch(e => e.send("Errors: "+e))
-            .then(m => {
-                message.ireply("", { embed: m })
-            }).catch(e => e.send("Errors: "+e))
+      if (!args[0]){
+        message.ireply("Send something to search, type `cancel` to cancel. You have 60s.")
+        message.channel.awaitMessages((msg) => msg.author.id === message.author.id, { max: 1, time: 60000 }).then(m => {
+          if (!m || m.first().content.toLowerCase() === "cancel") return;
+          args = m.first().content.toLowerCase().split(/\s+/)
+        })
+      }
+      let source = args.find(m => /^(--src|-s)\w*$/ig.test(m.trim().toLowerCase()))?.replace(/^(--src|-s)=(\w*)$/, "$2") ?? "stable";
+      let poss = ["master", "stable", "collection", "commando", "rpc", "akairo", "akairo-master"]
+      if (!poss.includes(source.toLowerCase())) return message.ireply("The source doesn't match the available ones:\n`"+poss.join("` `")+"`"); 
+      fetch(`https://djsdocs.sorta.moe/v2/embed?src=${source}&q=${args[0]}`)
+      .then(res => res.json()).catch(e => e.send("Errors: "+e))
+      .then(m => {
+        message.ireply("", { embed: m })
+      }).catch(e => e.send("Errors: "+e))
     }catch (e) {
       error.send("Errors:"+e.stack)
     }
