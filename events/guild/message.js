5ce355a008695055b9830e0193e6f46b94ef0ebe
heynaberuy <77380166+heynaberuy@users.noreply.github.com>
2021-05-12 08:12:30 +0300

    Update message.js
    fix

--- events/guild/message.js
+++ events/guild/message.js
@@ -3,8 +3,9 @@ const util = require("../../utils/CMDmismatch");
 
 module.exports = (client, message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
+  if (message.channel.type === "dm") return;
 
-  const args = message.content.toLowerCase().slice(prefix.length).split(/\s+/);
+  const args = message.content.toString().slice(prefix.length).split(/\s+/);
   const cmd = args.shift();
 
   const command = client.commands.get(cmd);
