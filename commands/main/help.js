e2bb8907a48e0400c5b51a0b22020a9cb515f760
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2021-05-12 11:10:01 +0200

    Update help.js

--- commands/main/help.js
+++ commands/main/help.js
@@ -5,12 +5,12 @@ module.exports = {
   name: "help",
   async execute(client, message, args) {
     try {
-
-      let embed = new MessageEmbed()
-        .setTitle(`Help command`)
-        .setDescription(`Use the reactions below for more indepth help`)
-        .addField(client.commands, 'test', true)
-
+      for (let i = 0; i < client.commands.length; i++) {
+        let embed = new MessageEmbed()
+          .setTitle(`Help command`)
+          .setDescription(`Use the reactions below for more indepth help`)
+          .addField(client.commands[i], 'test', true)
+      }
       message.channel.send(embed).catch(e => error.send("Error:" + e.stack));
     } catch (e) {
       error.send("Errors:" + e.stack)
