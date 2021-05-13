94b8f54b8effdf7e600ab96c324008f639d77bc9
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2021-05-13 11:30:48 +0200

    Update npm.js

--- commands/main/npm.js
+++ commands/main/npm.js
@@ -6,15 +6,15 @@ module.exports = {
   name: "npm",
   description: "a ping command",
   help: "Run a ping command to get the latency",
-  expArgs: "There will be no arguments needed",
+  expArgs: "<Object to be searched>",
   minArgs: 0,
   maxArgs: 0,
   perms: [""],
   execute(client, message, args) {
     try {
-      fetch('https://registry.npmjs.org/'+args[0]).then(m => {
-        message.reply(JSON.stringify(m), { split: {char:''}})
-})
+      fetch('https://registry.npmjs.org/' + args[0]).then(m => {
+        message.reply(JSON.stringify(m), { split: { char: '' } })
+      })
     } catch (e) {
       error.send("Errors:" + e.stack)
     }
