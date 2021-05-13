4d493b5686b5954a0bc22750ba0357c6076c391c
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-13 14:48:18 +0530

    Create npm.js

---
+++ commands/main/npm.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js");
+const error = require('../../utils/error')
+const fetch = require('node-fetch')
+
+module.exports = {
+  name: "npm",
+  description: "a ping command",
+  help: "Run a ping command to get the latency",
+  expArgs: "There will be no arguments needed",
+  minArgs: 0,
+  maxArgs: 0,
+  perms: [""],
+  execute(client, message, args) {
+    try {
+      fetch('http://registry.npmjs.org/'+args[0]).then(m => {
+        message.reply(JSON.stringify(m), { split: {char:''}})
+})
+    } catch (e) {
+      error.send("Errors:" + e.stack)
+    }
+  },
+};
