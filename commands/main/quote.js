a75c9c8fdec425b24c476a692c9971d3f79489ea
heynaberuy <>
2021-05-15 19:24:47 +0000

    Add quote file

---
+++ commands/main/quote.js
@@ -0,0 +1,16 @@
+const quote = require("../../utils/quotes.json");
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+  name: "quote",
+  description: "See a quote of pro coders",
+  help: "This command will send you a random quote",
+  expArgs: "There will be no arguments needed",
+  async execute(client, message, args) {
+    let qte = quote[Math.floor(Math.random() * quote.length)]
+
+    let embed = new MessageEmbed()
+      .setTitle("Random Quote")
+      .setDescription(`**|** ${qte.en} - ${qte.author}`)
+  }
+}
