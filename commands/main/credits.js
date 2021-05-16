7cfd441c9e5522fd81a24f3b99727f48250fb88f
heynaberuy <>
2021-05-16 14:45:26 +0000

    fix

--- commands/main/credits.js
+++ commands/main/credits.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
   name: "credits",
   description: "Send you developers and info of this bot",
-  help: "Sends this help command",
+  help: "Sends this command",
   expArgs: "There will be no arguments needed",
   async execute(client, message, args) {
       let embed = new MessageEmbed()
