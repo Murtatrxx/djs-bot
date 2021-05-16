e5ae76813daa0afa2a3674e856f02f3c880d2665
heynaberuy <>
2021-05-16 13:01:32 +0000

    edit02

--- commands/main/codehelp.js
+++ commands/main/codehelp.js
@@ -8,10 +8,10 @@ module.exports = {
   expArgs: "<language> <level>",
   async execute(client, message, Discord) {
     try {
-      const html_emoji = client.emojis.cache.get("843196462337622087")
-      const CSS_emoji = client.emojis.cache.get("843203296038092822")
-      const javascript_emoji = client.emojis.cache.get("843196462329364490")
-
+      const html_emoji = client.emojis.cache.get("843196462337622087");
+      const CSS_emoji = client.emojis.cache.get("843203296038092822");
+      const javascript_emoji = client.emojis.cache.get("843196462329364490");
+      const sass_emoji = client.emojis.cache.get("843473086530977812");
 
       const selectorembed = new MessageEmbed()
 
@@ -24,7 +24,7 @@ module.exports = {
         await embed.react(html_emoji);
         await embed.react(CSS_emoji);
         await embed.react(javascript_emoji);
-
+        await embed.react(sass_emoji);
 
         const htmlfilter = (reaction, user) => {
           return reaction.emoji === html_emoji && !user.bot;
