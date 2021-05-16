68491c5f9b41c1ebc384d4388b3b766fc196fd50
heynaberuy <>
2021-05-16 14:52:01 +0000

    .

--- commands/main/credits.js
+++ commands/main/credits.js
@@ -8,7 +8,7 @@ module.exports = {
   async execute(client, message, args) {
       let embed = new MessageEmbed()
         .setTitle("Developers of This Bot")
-        .setDescription("▫ <@756393473430519849> : Project Idea & Programmer \n ▫ <@432217612345278476> : Senior Programmer")
+        .setDescription("▫ <@756393473430519849> : Project Idea & Programmer \n ▫ <@432217612345278476> : Senior Programmer  \n ▫ <@830394727684898856> : Source Founder & JSON Code Editor & Programmer")
         .setColor("BLACK")
 
       message.channel.send(embed);
