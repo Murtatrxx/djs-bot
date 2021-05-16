33d00093bd0b0e9326a20b7fcb40fa9536e618f4
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-16 20:36:03 +0530

    minor-test001

--- commands/main/npm.js
+++ commands/main/npm.js
@@ -12,11 +12,34 @@ module.exports = {
   perms: [""],
   execute(client, message, args) {
     try {
-      fetch('http://registry.npmjs.org/'.concat(args[0])).then(res => res.json()).then(m => {
-        let data = JSON.stringify(m);
-        error.send(m)
-        error.send(data + args[0])
-        message.reply(data, { split: { char: '' } })
+      fetch('https://registry.npmjs.org/'.concat(args[0])).then(res => res.json()).then(m => {
+        
+        let em = new MessageEmbed()
+          .setTitle('404 Not Found')
+          .setColor("RED")
+          .setDescription("Your query: \""+args[0]+"\" was not found on NPM")
+          .setTimestamp()
+
+        if (m.error) return message.ireply("", { embed: em })
+
+        let {
+          name,
+          author = {},
+          repository = {},
+          homepage,
+          license,
+          description,
+          "dist-tags" : dtags = {},
+          _id
+        } = m
+
+        em.setTitle(name)
+        .setColor("RED")
+        .setURL("https://npmjs.com/"+_id)
+        .setTimestamp()
+        .setAuthor(author.name ?? "Unknown person")
+        .addFields({ name: 'Home page'})
+        message.ireply("", { embed: em })
       })
     } catch (e) {
       error.send("Errors:" + e.stack)
