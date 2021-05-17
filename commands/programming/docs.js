c1288450464877b1b612ea3c0d81e6eab8b8354f
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-17 13:00:27 +0530

    quote image test001

--- commands/programming/docs.js
+++ commands/programming/docs.js
@@ -12,7 +12,7 @@ module.exports = {
       if (!args[0]) {
         message.ireply("Send something to search, type `cancel` to cancel. You have 60s.")
         let m = await message.channel.awaitMessages((msg) => msg.author.id === message.author.id, { max: 1, time: 60000 })
-        if (!m || m.first().content.toLowerCase() === "cancel") return;
+        if (m?.first().content.toLowerCase() === "cancel") return;
         args = m.first().content.toLowerCase().split(/\s+/)
       }
       let source = args.find(m => /^(--src|-s)\w*$/ig.test(m.trim().toLowerCase()))?.replace(/^(--src|-s)=(\w*)$/, "$2") ?? "stable";
--- commands/programming/quote.js
+++ commands/programming/quote.js
@@ -1,5 +1,29 @@
+// @ts-check
 const quote = require("../../utils/quotes.json");
 const { MessageEmbed } = require('discord.js');
+const Canvas = require('canvas');
+
+
+const font = (c, qte) => {
+  const ctx = c.getContext("2d")
+
+  let txt = qte.en.match(/(.|\s){1, 25}/g)?.join("\n"),
+  fontSize = 35, measure, height, font = 50
+
+  do {
+
+    measure = ctx.measureText(txt)
+    fontSize -= 5
+
+  } while(measure.width > c.width - 300 || measure.height > c.height - 300)
+
+  do font -= 5;
+  while (ctx.measureText(qte.author).width > 180)
+
+  height = 410 + (fontSize / 2);
+
+  return { fnt: `${fontSize}px sans-serif`, txt: txt, author: { name: "- " + qte.author, height, fnt: `${font}px sans-serif`} }
+}
 
 module.exports = {
   name: "quote",
@@ -7,14 +31,39 @@ module.exports = {
   help: "This command will send you a random quote",
   expArgs: "There will be no arguments needed",
   async execute(client, message, args) {
+
     let qte = quote[Math.floor(Math.random() * quote.length)]
+    
+    const canvas = Canvas.createCanvas(800, 800);
+    const ctx = canvas.getContext('2d');
+    
+    ctx.rect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = "#FFC300"
+    ctx.fill()
+    
+    ctx.strokeStyle = '#74037b';
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+    let {
+      fnt,
+      txt,
+      author
+    } = font(canvas, qte)
+
+    ctx.fillStyle = "#111111"
+
+    ctx.font = fnt
+    ctx.fillText(txt, canvas.width / 2, canvas.height / 2)
+        
+    ctx.font = author.fnt
+    ctx.fillText(author.name, 600, author.height)
+
 
     let embed = new MessageEmbed()
       .setColor("RANDOM")
       .setTimestamp()
-      .setTitle("Quote")
-      .setDescription(`${qte.en} - **${qte.author}**`)
-
-    message.channel.send(embed);
+      .attachFiles([{attachment: canvas.toBuffer(), name: "quote.jpg"}])
+      .setImage("attachment://quote.jpg")
+    message.ireply("", { embed: embed });
   }
 }
