3181f39b638ef72edd183b18e723052203b8c335
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-18 17:47:26 +0530

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,5 @@
 const mongo = require("../../mongo")
+const serverSettingsSchema = require("../../Schema/serversettings")
 
 module.exports = async (client, Discord) => {
   console.log(`${client.user.tag} is ready`);
@@ -6,6 +7,7 @@ module.exports = async (client, Discord) => {
   await mongo().then(mongoose => {
     try {
       console.log(`Connected to MongoDB`)
+      serverSettingsSchema.find({})
     } catch (e){
       console.log(e)
     }
