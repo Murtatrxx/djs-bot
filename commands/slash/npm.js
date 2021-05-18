8bfea49d7cf7c0609e4563639fc28fa516e306da
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-18 21:37:06 +0530

    test001

--- commands/slash/npm.js
+++ commands/slash/npm.js
@@ -11,7 +11,43 @@ module.exports = {
         }
     ],
     async run(client, interaction){
+        try {
+            fetch('https://registry.npmjs.org/'.concat(interaction.data.options.find(m => m.name === 'query').value)).then(res => res.json()).then(m => {
+            
+            let em = new MessageEmbed()
+            .setTitle('404 Not Found')
+            .setColor("RED")
+            .setDescription("Your query: \""+args[0]+"\" was not found on NPM")
+            .setTimestamp()
+            
+            if (m.error) return interaction.reply("", { embed: em, flags: 64 })
+            
+            let {
+          name,
+          author = {},
+          repository = {},
+          homepage,
+          license,
+          description = "",
+          "dist-tags" : dtags = {},
+          _id,
+          time
+        } = m
+        time = Object.values(time)
         
+        em.setTitle(name+"@"+dtags.latest)
+        .setColor("RED")
+          .setDescription(description)
+          .setURL("https://npmjs.com/"+_id)
+          .setTimestamp()
+          if (homepage) em.addFields({ name: 'Home page', value: homepage })
+          if (license) em.addFields({ name: 'License', value: license })
+          if (time.length) em.addFields({ name: 'Last Publish', value: time[time.length - 1] })
+          interaction.reply("", { embed: em })
+        })
+    } catch (e) {
+        error.send("Errors:" + e.stack);
     }
-
+    }
+    
 }
--- commands/slash/ping.js
+++ commands/slash/ping.js
@@ -4,7 +4,11 @@ module.exports = {
     name: 'ping',
     des: 'Pong..!',
     async run(client, interaction){
-        
+        try {
+            interaction.reply("WS ping: "+client.ws.ping+"ms", { flags: 64 })
+        } catch (e) {
+            error.send("Errors:" + e.stack)
+        }
     }
 
 }
