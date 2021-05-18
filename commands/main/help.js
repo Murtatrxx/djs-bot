ef5ae7efe55a19d5dcf1aed6764a3b7d8f17b2a4
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-18 09:53:19 +0530

    help-test001

--- commands/main/help.js
+++ commands/main/help.js
@@ -6,15 +6,29 @@ module.exports = {
   name: "help",
   description: "Sends this message",
   help: "Sends this help command",
-  expArgs: "There will be no arguments needed",
+  expArgs: "",
   async execute(client, message, args) {
     try {
       let embed = new MessageEmbed()
-        .setTitle(`Help command`)
-        .setThumbnail(client.user.displayAvatarURL())
-        .setColor("GREEN")
-        .setDescription(client.commands.map(c => `\`${prefix}${c.name}\` - ${c.description}`).join('\n'))
-      message.channel.send(embed).catch(e => error.send("Error:" + e.stack));
+      .setTitle(`Help command`)
+      .setThumbnail(client.user.displayAvatarURL())
+      .setColor("GREEN")
+      .setTimestamp()
+      .setDescription(client.commands.map(c => `\`${prefix}${c.name}\` - ${c.description}`).join('\n'))
+      if (!args[0]) return message.channel.send(embed).catch(e => error.send("Error:" + e.stack))
+      let cmd = client.commands.get(args[0].toLowerCase())
+      if (!cmd) {
+        embed.setColor("RED")
+          .setDescription("Requested command was not found")
+          .setTitle("404 Not Found")
+        return message.ireply("", { embed: embed })
+      }else{
+        embed.setTitle(cmd.name)
+          .setDescription(cmd.help)
+        if (cmd.expArgs) embed.addFields({ name: 'Expected args', value: `\`\`\`${cmd.expArgs}\`\`\`` })
+        message.ireply("", { embed: embed, mention: true })
+      }
+
 
     } catch (e) {
       error.send("Errors:" + e.stack)
