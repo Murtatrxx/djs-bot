a72ea5151649f26c05aa87ce4155b57f1e776e29
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-18 14:48:30 +0530

    test006

--- commands/programming/node.js
+++ commands/programming/node.js
@@ -7,16 +7,10 @@ const base = 'https://nodejs.org';
 let data = null;
 
 module.exports = {
-    name: 'eval',
-    des: 'search the node docs',
-    options: [
-        {
-            name: 'query',
-            required: true,
-            type: 4
-        }
-    ],
-    async run(client, interaction){
+    name: 'node',
+    
+    async run(client, message, args) {
+
         const findRec = (o, name, type, module) => {
             name = name.toLowerCase();
             if (!module) module = o?.type === 'module' ? o?.name.toLowerCase() : undefined;
@@ -39,54 +33,53 @@ module.exports = {
 		
 		
 		function anchor(text, module) {
-			const method = text
+            const method = text
 			.toLowerCase()
 			.replace(/ |`|\[|\]|\)/g, '')
 			.replace(/\.|\(|,|:/g, '_');
 			return `${module}#${method}`;
 		}
 		
-(async (query)  => {
-	try {
-		if (!data) {
+        (async (query)  => {
+            try {
+        if (!data) {
             //@ts-ignore
-			data = await fetch(`${base}/dist/latest/docs/api/all.json`).then(r => r.json());
+			data = await fetch(`${API_BASE}/dist/latest/docs/api/all.json`).then(r => r.json());
 		}
-
+        
 		const queryParts = query.split(/#|\./);
 		const altQuery = queryParts[queryParts.length - 1];
-
+        
 		const result =
-			findRec(data, query, 'class') ??
-			findRec(data, query, 'classMethod') ??
-			findRec(data, query, 'method') ??
-			findRec(data, query, 'event') ??
-			findRec(data, altQuery, 'class') ??
-			findRec(data, altQuery, 'method') ??
-			findRec(data, altQuery, 'event') ??
-			findRec(data, altQuery, 'classMethod') ??
-			findRec(data, query, 'module') ??
-			findRec(data, altQuery, 'module');
-
+        findRec(data, query, 'class') ??
+        findRec(data, query, 'classMethod') ??
+        findRec(data, query, 'method') ??
+        findRec(data, query, 'event') ??
+        findRec(data, altQuery, 'class') ??
+        findRec(data, altQuery, 'method') ??
+        findRec(data, altQuery, 'event') ??
+        findRec(data, altQuery, 'classMethod') ??
+        findRec(data, query, 'module') ??
+        findRec(data, altQuery, 'module');
+        
 		if (!result) {
-			return interaction.reply(`No result found for query \`${query}\`.`, { flags: 64 });
+			return message.ireply(`No result found for query \`${query}\`.`, { flags: 64 });
 		}
-
+        
 		const moduleURL = `${base}/api/${result.module}`;
 		const fullURL = `${moduleURL}.html${result.type === 'module' ? '' : `#${anchor(result.textRaw, result.module)}`}`;
 		const parts = [`[**${result.textRaw}**](<${fullURL}>)`];
-
+        
 		const intro = (result.desc ?? 'no intro').split('\n\n')[0];
 		const linkReplaceRegex = /\[(.+?)\]\((.+?)\)/g;
 		const boldCodeBlockRegex = /`\*\*(.*)\*\*`/g;
-
+        
 		parts.push(intro.replace(linkReplaceRegex, `[$1](<${base}/$2>)`).replace(boldCodeBlockRegex, '**`$1`**'));
-
-		interaction.reply(`${parts.join('\n')}`);
+        
+		message.ireply(`${parts.join('\n')}`);
 	} catch (e) {
-
-	}
-})(interaction.data.options.find(m => m.name === 'query').value)
+        
     }
-
+})(args.join(" "))
+}
 }
--- commands/slash/mdn.js
+++ commands/slash/mdn.js
@@ -1,4 +1,15 @@
-const { MessageEmbed } = require('discord.js')
+// @ts-check
+const { MessageEmbed } = require("discord.js");
+const error = require('../../utils/error')
+const fetch = require("node-fetch")
+const { encode } = require("querystring")
+
+const cache = new Map()
+const base = "https://developer.mozilla.org/"
+const em = new MessageEmbed()
+    .setColor("GREEN")
+    .setTimestamp()
+    .setAuthor("mdn docs", "https://developer.mozilla.org/favicon-48x48.97046865.png", "https://developer.mozilla.org/")
 
 module.exports = {
     name: 'mdn',
@@ -11,7 +22,35 @@ module.exports = {
         }
     ],
     async run(client, interaction){
+        try {
+            const findDoc = async (query, message) => {
+                let uri = base + "api/v1/search?" + encode({ q: query })
+                let data = cache.get(uri)
+                if (!data) {
+                    // @ts-ignore
+                    let res = await fetch(uri).then(res => res.json()).catch(e => error.send("Error: " + e.stack))
+                    data = res.documents;
+                    cache.set(uri, data)
+                }
 
+                if (!data?.length) return message.reply("No docs found for: " + query, { flags: 64 })
+
+                let pre = data[0].summary
+                    .replace(/\s+/g, " ")
+                    .replace(/\[(.+?)\]\((.+?)\)/g, `[$1](${base}<$2>)`)
+                    .replace(/`\*\*(.*)\*\*`/g, "**`$!`**")
+
+                em.setTitle(data[0].title)
+                    .setURL(base + data[0].mdn_url)
+                    .setDescription(pre.substr(0, 2000))
+                if (data.length > 1) em.addFields({ name: 'Did you mean?', value: data.slice(1, 6).map(m => `[${m.title}](${base + m.mdn_url})`).join('\n') })
+
+                message.reply("", { embed: em }).catch(e => error.send("Error: " + e.stack))
+            }
+            findDoc(interaction.data.options.find(m => m.name === 'query').value.trim(), interaction)
+        } catch (e) {
+            error.send("Errors:" + e.stack)
+        }
     }
 
 }
