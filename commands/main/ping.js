0676d10fabc473c368854587abefad8040c1fffc
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-19 16:56:04 +0530

    Update ping.js

--- commands/main/ping.js
+++ commands/main/ping.js
@@ -10,10 +10,10 @@ module.exports = {
     try {
       message.channel.send("Pong!").then((m) => {
         let ping = m.createdTimestamp - message.createdTimestamp;
-        const ememem = new MessageEmbed().setColor("PURPLE").addFields({
+        const ememem = new MessageEmbed().setColor("PURPLE").setTitle('Stats').setURL('https://discord.com/api/oauth2/authorize?client_id=838864249958301706&permissions=8&scope=applications.commands%20bot').addFields({
           name: `**:hourglass: | Ping: ${ping}ms **`,
           value: `**:heartbeat: | API: ${client.ws.ping}ms **`,
-        });
+        }).setDescription('Guilds: '+client.guilds.cache.size);
         if (ping >= 1000) return m.edit("", { embed: errembed });
         m.edit("", { embed: ememem }).catch(e => error.send("Error:" + e.stack));
       }).catch(e => error.send("Error:" + e.stack));
