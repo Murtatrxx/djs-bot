f74f7f1be305a63de472b6e8b8064f4d88606eab
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-19 11:34:50 +0530

    minor

--- commands/programming/math.js
+++ commands/programming/math.js
@@ -6,26 +6,27 @@ module.exports = {
   help: "Math!",
   expArgs: "<experission>",
   async execute(client, message, Discord) {
-      let resp;
-      
-      try {
-        resp = math.evaluate(message.content)
-      } catch(e) {
-        let a = "```"
-        return message.channel.send(`I think you entered the math incorrectly :c ${a}${e}${a}`)
-      }
-      
-      let g = await message.channel.send("Calculating ...")
-      g.delete()
     
-  
-        const embed = new MessageEmbed()
-        .setColor(color)
-        .setTitle('Calculator')
-        .addField('Question', `\`\`\`css\n${text}\`\`\``)
-        .addField('Answer', `\`\`\`css\n${resp}\`\`\``)
-        .setFooter(`⌚ Speed: ${g.createdTimestamp - message.createdTimestamp}ms`)
-
-        message.channel.send(embed);
+    if (message.guild.id === '839102795327864852') return;
+    let resp;
+    try {
+      resp = math.evaluate(message.content)
+    } catch(e) {
+      let a = "```"
+      return message.channel.send(`I think you entered the math incorrectly :c ${a}${e}${a}`)
+    }
+    
+    let g = await message.channel.send("Calculating ...")
+    g.delete()
+    
+    
+    const embed = new MessageEmbed()
+    .setColor(color)
+    .setTitle('Calculator')
+    .addField('Question', `\`\`\`css\n${text}\`\`\``)
+    .addField('Answer', `\`\`\`css\n${resp}\`\`\``)
+    .setFooter(`⌚ Speed: ${g.createdTimestamp - message.createdTimestamp}ms`)
+    
+    message.channel.send(embed);
   }
 }
