d96658d662fae0883665df977170b32f04de4186
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-05-19 07:37:25 +0530

    Update npm.js

--- commands/slash/npm.js
+++ commands/slash/npm.js
@@ -8,7 +8,7 @@ module.exports = {
             name: 'query',
             required: true,
             description: 'Name of the package',
-            type: 4
+            type: 3
         }
     ],
     async run(client, interaction){
